036a9f9b07064793598e722c8e989e24b70a852f
Andrei Valdez <65985776+andreivaldezau@users.noreply.github.com>
2025-06-24 21:01:51 +1000

    Refactor to addRow and addSquare functions

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const GRID_LENGTH = 16;
-
 const container = document.querySelector(".container");
 
 for (let i = 0; i < GRID_LENGTH; i++) {
+  const row = addRow();
+  for (let j = 0; j < GRID_LENGTH; j++) {
+    addSquare(row);
+  }
+}
+
+function addRow() {
   const row = document.createElement("div");
   row.classList.add("row");
-  container.appendChild(row);
-  for (let j = 0; j < GRID_LENGTH; j++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    row.appendChild(square);
-  }
+  return container.appendChild(row);
+}
+
+function addSquare(row) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+  row.appendChild(square);
 }
