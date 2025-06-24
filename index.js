b952df78d1f6483353e34664af5948f938b85362
Andrei Valdez <65985776+andreivaldezau@users.noreply.github.com>
2025-06-24 17:37:54 +1000

    Add row logic

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const GRID_LENGTH = 16;
 const container = document.querySelector(".container");
 
 for (let i = 0; i < GRID_LENGTH; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  container.appendChild(square);
+  const row = document.createElement("div");
+  row.classList.add("row");
+  container.appendChild(row);
+  for (let j = 0; j < GRID_LENGTH; j++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    row.appendChild(square);
+  }
 }
